d35762ab39fdc9958894953fa12b90c90457bda2
avk1702 <avk1702@bk.ru>
2023-02-21 11:14:53 +0500

    решение методом bool

--- task_19/Program.cs
+++ task_19/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+bool Palindrom(string number)
+{
+    int index = number.Length;
+    if (index == 5)
+    {
+        if (number[0] == number[4] && number[1] == number[3])
+        {
+            Console.WriteLine(" Палиндром ");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine(" Не палиндром ");
+            return false;
+        }
+        return true;
+    }
+    else
+    {
+       Console.WriteLine("Это не 5-ти значное число: "); 
+    }
+    return false;
+}
+
+Console.WriteLine("Введите 5-ти значное число: ");
+string number = Console.ReadLine();
+
+bool result = Palindrom(number);
+Console.WriteLine(result);
