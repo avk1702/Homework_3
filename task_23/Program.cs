1bedc46b6cb59c175454c494bd2df2bcc597d4dc
avk1702 <avk1702@bk.ru>
2023-02-22 16:16:25 +0500

    решение с void Main

--- task_23/Program.cs
+++ task_23/Program.cs
@@ -1,17 +1,23 @@
 ﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу
 // кубов чисел от 1 до N.3 -> 1, 8, 27. 5 -> 1, 8, 27, 64, 125
+internal class Program
+{
+  private static void Main(string[] args)
+  {
     void Cube(int number)
     {
       int countdown = 1;
       int index = 0;
       while (index < number)
       {
-        Console.Write(Math.Pow(countdown,3) + "  ");
+        Console.Write(Math.Pow(countdown, 3) + "  ");
         index++;
         countdown++;
       }
     }
-Console.WriteLine("Введите N  ");
-int number = Convert.ToInt32(Console.ReadLine());
-Cube(number);
-Console.WriteLine();
+    Console.WriteLine("Введите N  ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Cube(number);
+    Console.WriteLine();
+  }
+}
